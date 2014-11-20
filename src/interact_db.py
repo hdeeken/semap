ec3c3a36a24f20ed4c8ff0af941eb6266cd2ee78
Henning Deeken <hdeeken@uos.de>
2014-11-20 18:58:10 +0100

    obj instance / distance query example

--- src/interact_db.py
+++ src/interact_db.py
@@ -4,87 +4,111 @@ import roslib; roslib.load_manifest('spatial_db')
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.sql import func
-from geoalchemy2.elements import (
-    WKTElement, WKBElement, RasterElement, CompositeElement)
-    
-from geoalchemy2.functions import ST_Distance, ST_AsText
-
-from db_environment import Session
-from db_model import Object, Pose
-
-from geometry_msgs.msg import PoseStamped as ROSPose
-
-session = Session()
-#session.add_all([
-                 #Object(name='object1', type='Test', pose = Pose(ref_system = "map", pose_string = '0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0')),
-                 #Object(name='object2', type='Test', pose = Pose(ref_system = "map", pose_string = '1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0')),
-                 #Object(name='object3', type='Test', pose = Pose(ref_system = "map", pose_string = '0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0')),
-                 #Object(name='object4', type='Test', pose = Pose(ref_system = "map", pose_string = '0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0'))])
-
-#session.add(Object(name='object6', type='Test', pose = Pose(ref_system = "map", pose_string = '1.0, 3.0, 4.0, 0.0, 0.0, 0.0, 1.0')))
-#session.add(Object(name='object7', type='Test', pose = Pose(ref_system = "map", pose_string = '1.0, 3.0, 4.0, 0.0, 0.0, 0.0, 1.0')))
-#session.commit()
-
-for obj in session.query(Object).\
-    filter(Object.type == 'Test').\
-    filter(Object.geo_absolute_position!=None): 
-    print obj.name
-    print obj.absolute_position()
-
-#session.commit()
-
-#print 'skskskskskksksksksk'
-
-##for obj in session.query(Object).filter(Object.name == 'object1'):
-## print '###'
-## print obj.absolute_position
-## print '###'
-## print obj.geo_absolute_position
-
-#obj1 = session.query(Object).filter(Object.name == 'object1')
-#obj2 = session.query(Object).filter(Object.name == 'object4')
-
-##print obj1
-
-##for r in session.execute(func.ST_Distance(obj1[0].geo_absolute_position, obj2[0].geo_absolute_position)):#
-##	print r
+from sqlalchemy.orm import aliased
 
 from geoalchemy2 import Geometry
 from geoalchemy2.functions import GenericFunction
+from geoalchemy2.elements import WKTElement, WKBElement, RasterElement, CompositeElement
+from geoalchemy2.functions import ST_Distance, ST_AsText
+
+from db_environment import Session
+from db_model import *
+
+from geometry_msgs.msg import PoseStamped as ROSPose
+
+class cos(GenericFunction):
+    name = 'cos'
+    type = None
+
+class sin(GenericFunction):
+    name = 'sin'
+    type = None
+
+class pi(GenericFunction):
+    name = 'pi'
+    type = None
 
 class ST_3DDistance(GenericFunction):
     name = 'ST_3DDistance'
     type = None
 
-from sqlalchemy.orm import aliased
-o1 = aliased(Object)
-o2 = aliased(Object)
-#for  object1, object2, distance2D, distance3D in \
-  #session.query(adalias1, adalias2, func.ST_Distance(o1.absolute_position, o2.absolute_position), func.ST_3DDistance(o1.geo_absolute_position, o2.geo_absolute_position)).\
-  #filter(o1.name=='object6').\
-  #filter(o2.name=='object7'):
-  #print 'Object1:', object1.name
-  #print 'Object2:', object2.name
-  #print 'Dist2D: ', distance2D
-  #print 'Dist3D: ', distance3D
+class ST_3DIntersection(GenericFunction):
+    name = 'ST_3DIntersection'
+    type = None
 
-#obs2 = session.query(Object).filter(Object.name == 'object6')
+class ST_Affine(GenericFunction):
+    name = 'ST_Affine'
+    type = None
 
-#for i in session.query(Object).filter(Object.name == 'object1'):
-	#print i
-	#print i.name
-	#print i.geo_absolute_position
-	#print i.absolute_position()
+session = Session()
 
-#print obs2[0].name, '\n'
-#print obs2[0].absolute_position
-#session.commit()
-#print obs2[0].absolute_position
+def test_object_query():
+    o1 = aliased(ObjectInstance)
+    o2 = aliased(ObjectInstance)
+    for  object1, object2 in \
+      session.query(o1, o2).\
+      filter(o1.alias=='5').\
+      filter(o2.alias=='6'):
+       print 'Object 1'
+       print 'Alias:', object1.alias
+       print 'Type:',object1.object_description.type
+       print '2D Model Type:',object1.object_description.geometry_model2d.model_type
+       print '2D Geo Type:',object1.object_description.geometry_model2d.geometry_type
+       print '2D Geo:', object1.object_description.geometry_model2d.geometry
+       print '3D Model Type:',object1.object_description.geometry_model3d.model_type
+       print '3D Geo Type::',object1.object_description.geometry_model3d.geometry_type
+       print '3D Geo:', object1.object_description.geometry_model3d.geometry
+       print 'Object 2'
+       print 'Alias:', object2.alias
+       print 'Type:',object2.object_description.type
+       print '2D Model Type:',object2.object_description.geometry_model2d.model_type
+       print '2D Geo Type:',object2.object_description.geometry_model2d.geometry_type
+       print '2D Geo:', object2.object_description.geometry_model2d.geometry
+       print '3D Model Type:',object2.object_description.geometry_model3d.model_type
+       print '3D Geo Type::',object2.object_description.geometry_model3d.geometry_type
+       print '3D Geo:', object1.object_description.geometry_model3d.geometry
 
-#for dist in session.execute(func.ST_Distance(obs1[0].absolute_position(), obs2[0].absolute_position())):
-#    print dist
+       for r in session.execute(ST_AsText(ST_3DIntersection(object1.object_description.geometry_model3d.geometry, object2.object_description.geometry_model3d.geometry))):
+           print r
+       for r in session.execute(ST_AsText(ST_Affine(object1.object_description.geometry_model3d.geometry,  cos(pi()), -sin(pi()), 0,  sin(pi()), cos(pi()), 0,  0, 0, 1,  0, 0, 0))):
+           print r
+       for r in session.execute(func.ST_Distance(object1.object_description.geometry_model2d.geometry, object2.object_description.geometry_model2d.geometry)):
+        print r
+       for r in session.execute(func.ST_3DDistance(object1.object_description.geometry_model3d.geometry, object2.object_description.geometry_model3d.geometry)):
+        print r
 
-#
+def create_a_dummy_object_instance():
+    geo2d = GeometryModel2D()
+    geo2d.model_type = 'position2d'
+    geo2d.geometry_type = 'POINT'
+    geo2d.geometry = WKTElement('POINT(0 0)')
+    geo3d = GeometryModel3D()
+    geo3d.model_type = 'primitive3d'
+    geo3d.geometry_type = 'POINTZ'
+    #geo3d.geometry_type = 'LINESTRINGZ'
+    #geo3d.geometry_type = 'POLYGONZ'
+    geo3d.geometry = WKTElement('POINT(0 0 0)')
+    #geo3d.geometry = WKTElement('LINESTRING Z (2 2 6,1.5 1.5 7,1 1 8,0.5 0.5 8,0 0 10)')
+    #geo3d.geometry = WKTElement('POLYGON((0 0 8, 0 1 8, 1 1 8, 1 0 8, 0 0 8))')
 
-#for user, post in query:
-#    print user.user_id, post.post_id
+    obj_desc = ObjectDescription()
+    obj_desc.type = 'test_object'
+    obj_desc.geometry_model2d = geo2d
+    obj_desc.geometry_model3d = geo3d
+
+    pose = Pose()
+    pose.ref_system = 'origin'
+    pose.pose = '1,2,1,0,0,0,1'
+
+    obj_inst = ObjectInstance()
+    obj_inst.alias = '6'
+    obj_inst.pose = pose
+    obj_inst.object_description = obj_desc
+
+    session.add(obj_inst)
+    session.commit()
+
+if __name__ == "__main__":
+  #  create_a_dummy_object_instance()
+    test_object_query()
+    print 'done'
