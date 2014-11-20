f9c36864b8bb6257bb61be871b02cdb167d3fb5d
Henning Deeken <hdeeken@uos.de>
2014-11-20 18:50:29 +0100

    restructured includes of create and clear scripts

--- src/clear_db.py
+++ src/clear_db.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import roslib; roslib.load_manifest('spatial_db')
-
 from sqlalchemy import Column, Integer, String
-from db_environment import Base
-from db_model import Object 
-
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://hdeeken:mYdB@localhost/sfcgal_test', echo=True)
 
+from db_environment import Base
+from db_model import *
+
+engine = create_engine('postgresql://hdeeken:mYdB@localhost/sfcgal_test', echo=True)
 Base.metadata.drop_all(engine)
