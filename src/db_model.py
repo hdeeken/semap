d33ff8658264e219ca03dc342244c90519bc6cee
Henning Deeken <hdeeken@uos.de>
2014-11-20 18:52:19 +0100

    first draft: object description vs instance schema
    created a first draft on how to define
    
    a) an unrestricted set of 2d / 3d geometries
    b) which can be bound to a geometric 'object descriptions'
    c) that can be instanciated by combining a pose with an object description

--- src/db_model.py
+++ src/db_model.py
@@ -11,17 +11,89 @@ from geometry_msgs.msg import PoseStamped as ROSPose
 
 from db_environment import Base
 
+# GeometryModel2D
+# holds the various possible 2d geometries that can be identified for an object
+# model_type: defines the purpose of this geometry (e.g. 2d footprint)
+# geometry_type: defines the underlying geometry (e.g. 2d polygon)
+# geometry: the geometry itself as binary blob
+
+class GeometryModel2D(Base):
+  __tablename__ = 'geometry_model2d'
+  id = Column('id', Integer, primary_key=True)
+  model_type = Column('model_type ', String)
+  geometry_type = Column('geometry_type ', String)
+  geometry = Column('geometry', Geometry(geometry_type='GEOMETRY', dimension=2), nullable=False)
+
+# GeometryModel3D
+# holds the various possible 3d geometries that can be identified for an object
+# model_type: defines the purpose of this geometry (e.g. 3d surface)
+# geometry_type: defines the underlying geometry (e.g. 3d triangle mesh)
+# geometry: the geometry itself as binary blob
+
+class GeometryModel3D(Base):
+  __tablename__ = 'geometry_model3d'
+  id = Column('id', Integer, primary_key=True)
+  model_type = Column('model_type ', String)
+  geometry_type = Column('geometry_type ', String)
+  geometry = Column('geometry', Geometry(geometry_type='GEOMETRYZ', dimension=3), nullable=False)
+
+# ObjectDescription
+# defines the geometric appearence of a object of a specific type, and is the blueprint object instances
+# type: defines the type of object (e.g. Table)
+# geometry_model2d: specifies all 2d geometric models bound to this object
+# geometry_model3d: specifies all 2d geometric models bound to this object
+# FUTURE: default model specifies which models are drawn on default
+
+class ObjectDescription(Base):
+  __tablename__ = 'object_description'
+  id = Column('id', Integer, primary_key=True)
+  type = Column('type', String)
+
+## must be in instance level
+##  active_model2d_id = Column('active_model2d_id', Integer, ForeignKey('geometry_model2d.id'), nullable=True)
+##  active_model2d = relationship("GeometryModel2D", backref=backref('object_description', uselist=False))
+# default is to be kept here but causes some kind of problem, needs further research
+#  default_model2d_id = Column('default_model2d_id', Integer, ForeignKey('geometry_model2d.id'), nullable=True)
+#  default_model2d = relationship("GeometryModel2D", backref=backref('object_description', uselist=False))
+  geometry_model2d_id = Column('geometry_model2d_id', Integer, ForeignKey('geometry_model2d.id'), nullable=True)
+  geometry_model2d = relationship("GeometryModel2D", backref=backref('object_description', uselist=True))
+## must be in instance level
+##  active_model3d_id = Column('active_model3d_id', Integer, ForeignKey('geometry_model3d.id'), nullable=True)
+##  active_model3d = relationship("GeometryModel3D", backref=backref('object_description', uselist=False))
+# default is to be kept here but causes some kind of problem, needs further research
+#  default_model3d_id = Column('default_model3d_id', Integer, ForeignKey('geometry_model3d.id'), nullable=True)
+#  #default_model3d = relationship("GeometryModel3D", backref=backref('object_description', uselist=False))
+  geometry_model3d_id = Column('geometry_model3d_id', Integer, ForeignKey('geometry_model3d.id'), nullable=True)
+  geometry_model3d = relationship("GeometryModel3D", backref=backref('object_description', uselist=True))
+
+# ObjectInstance
+# defines an instanciates object
+# alias: gives the opportunity of well-identifiable names
+# pose: gives the position and orientation of this object with respect to it's reference system
+# object_description: describes the object geometrically and is used in conjunction with the pose to locate the object within it's ref_system
+
+class ObjectInstance(Base):
+  __tablename__ = 'object_instance'
+  id = Column('id', Integer, primary_key=True)
+  alias = Column('alias', String, nullable=True)
+  # pose of the object
+  pose_id = Column('pose_id', Integer, ForeignKey('pose.id'), nullable=True)
+  pose = relationship("Pose", backref=backref('geometry_model', uselist=False))
+  # object description
+  object_description_id = Column('object_description_id', Integer, ForeignKey('object_description.id'), nullable=True)
+  object_description = relationship("ObjectDescription", backref=backref('object_instance', uselist=False))
+
 class Pose(Base):
   __tablename__ = 'pose'
-  id = Column(Integer, primary_key=True)
-  ref_system = Column(String)
-  pose_string = Column(String)
+  id = Column('id', Integer, primary_key=True)
+  ref_system = Column('ref_system', String)
+  pose = Column('pose', String)
 
   def setPose(self, float_array):
-    self.pose_string = ",".join(map(str, float_array))
+    self.pose = ",".join(map(str, float_array))
 
   def getPose(self):
-    return map(float, self.pose_string.split(','))
+    return map(float, self.pose.split(','))
 
   def toROS(self):
     rospose = ROSPose()
@@ -48,36 +120,3 @@ class Pose(Base):
       self.setPose(array)
       self.ref_system = rospose.header.frame_id
       return
-
-class Object(Base):
-  __tablename__ = 'object'
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
-  name = Column(String)
-  pose_id = Column(Integer, ForeignKey('pose.id'), nullable=True)
-  pose = relationship("Pose", backref=backref('object', uselist=False))
-  geo_absolute_position = Column(Geometry(geometry_type='POINTZ'), nullable=True) 
-  # default=set_absolute_position, onupdate=set_absolute_position)
-
-  def toROS(self):
-    rosobject = ROSPose()
-    rosobject.id = self.id
-    rosobject.type = self.type
-    rosobject.name = self.name
-    rosobject.pose = self.pose.fromROS()
-    return rosobject
-
-  def fromROS(self, rosobject):
-    self.type = rosobject.type
-    self.name = rosobject.name
-    pose = Pose()
-    pose.fromROS(rosobject.pose)
-    self.pose = pose
-    return
-  
-  def __repr__(self):
-    return "<Object(id='%d' type='%s', name='%s', pose_id='%d', pose='%s')>" % (
-      self.id, self.type, self.name, self.pose_id, self.pose.pose_string)
-
-  #def set_absolute_position(context):
-   # self.geo_absolute_position = WKTElement("POINTZ(%f %f %f)" % (context.current_parameters['pose'].getPose()[0], context.current_parameters['pose'].getPose()[1], context.current_parameters['pose'].getPose()[2]))
