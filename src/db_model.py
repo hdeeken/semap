7bcf2fce6f922df3e42352cbf8c1bc261ddeb4e1
Henning Deeken <hdeeken@uos.de>
2015-10-02 17:09:15 +0200

    export schema

--- src/db_model.py
+++ src/db_model.py
@@ -21,3 +21,37 @@ def truncate_all():
 
 def drop_all():
   Base.metadata.drop_all(engine())
+
+from sqlalchemy import MetaData
+from sqlalchemy_schemadisplay import create_schema_graph
+
+def write_graph():
+  # create the pydot graph object by autoloading all tables via a bound metadata object
+  graph = create_schema_graph(metadata=Base.metadata, #metadata=MetaData('postgres://user:pwd@host/database'),
+     show_datatypes=False, # The image would get nasty big if we'd show the datatypes
+     show_indexes=False, # ditto for indexes
+     rankdir='TB',# was LR # From left to right (instead of top to bottom)
+     concentrate=False # Don't try to join the relation lines together
+  )
+  graph.write_png('/home/hdeeken/dbschema.png') # write out the file
+
+from sqlalchemy_schemadisplay import create_uml_graph
+from sqlalchemy.orm import class_mapper
+
+def write_uml():
+  # lets find all the mappers in our model
+  mappers = []
+  for attr in dir(Base):
+      if attr[0] == '_': continue
+      try:
+          cls = getattr(Base, attr)
+          mappers.append(class_mapper(cls))
+      except:
+          pass
+
+  # pass them to the function and set some formatting options
+  graph = create_uml_graph(mappers,
+      show_operations=False, # not necessary in this case
+      show_multiplicity_one=False # some people like to see the ones, some don't
+  )
+  graph.write_png('/home/hdeeken/umlschema.png') # write out the file
